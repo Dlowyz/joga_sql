50b3494e4d2a84d1b8084646f924269cdf69d20c
Dlowyz <artur.lomp@voco.ee>
2024-10-28 23:12:34 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
-
+const app = express()
 const path = require('path')
 const hbs = require('express-handlebars');
+
 app.set('views', path.join(__dirname, 'views'));
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
@@ -10,11 +11,11 @@ app.engine('hbs', hbs.engine({
 }));
 
 app.use(express.static('public'));
-
+app.set('view engine', 'hbs');
 const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 
-const app = express()
+
 
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -22,13 +23,27 @@ const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'qwerty',
-    database: 'joga_mysql'
+    database: 'joga_mysql',
+    port: 3307
 })
 
 con.connect((err) => {
     if (err) throw err;
     console.log('Connected to joga_mysql db')
+    
 })
+
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = [];
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result;
+        res.render('index',{articles: articles});
+    });
+    
+});
+
 
 app.listen(3003, () => {
     console.log('App is started at http://localhost:3003')
